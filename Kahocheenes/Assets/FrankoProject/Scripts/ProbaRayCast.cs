6d7c6bf6f2b8eec719f990ba83b1a211e2323293
Renato Gracin <rg50894@fer.hr>
2021-12-26 12:50:43 +0100

    napokom pusham

---
+++ Kahocheenes/Assets/FrankoProject/Scripts/ProbaRayCast.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProbaRayCast : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+        public float wheelRadius;
+    public float suspensionRadius; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Debug.DrawRay(transform.position, -transform.up * (wheelRadius), Color.red);
+    }
+}
---
+++ Kahocheenes/Assets/FrankoProject/Scripts/VelocityScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelocityScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Rigidbody rb;
+    public Vector3 Velocity;
+    public Vector3 AngularVelocity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Velocity = rb.velocity;
+        AngularVelocity = rb.angularVelocity;
+        float fire1 = Input.GetAxis("Fire1");
+        if (fire1 > 0)
+        {
+            
+        }
+    }
+}
